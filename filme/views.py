cb058971aab3e4845fc16c414708b12a96ed4b99
Gabriel Beligolli <108367726+Beligolli@users.noreply.github.com>
2022-11-14 12:18:57 -0300

    first commit

---
+++ filme/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
+
+
+# Create your views here.
+# def homepage(request):
+#     return render(request, "homepage.html")
+
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+
+# Create your views here.
+#url - views - html
+# def homefilmes(request):
+#     context ={}
+#     lista_filmes = Filme.objects.all()
+#     context['lista_filmes'] = lista_filmes
+#     return render(request, "homefilmes.html", context)
+
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme
+
+class Detalhesfilme(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
